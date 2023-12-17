db994ffab84c4ed6212e2547b994af457d1340f4
Haddad Sarah <haddads686@gmail.com>
2023-12-17 13:24:10 +0100

    _

--- pareto.py
+++ pareto.py
@@ -1,22 +1,27 @@
 import numpy as np
 
-def pareto(matrix):
-    num_points, num_criteria = matrix.shape[:2]
-    is_dominated = np.zeros(num_points, dtype=bool)
+def is_dominated(a, b):
+    """Check if vector 'a' is dominated by vector 'b'."""
+    return (a[0] < b[1] and a[1] <= b[1]) or (a[0] <= b[0] and a[1] < b[1])
 
-    for i in range(num_points):
-        for j in range(num_points):
-            if i != j and np.all(matrix[i] <= matrix[j]):
-                is_dominated[i] = True
+def find_pareto_optimal(matrix):
+    """Find Pareto optimal points in a given matrix."""
+    flatmat = matrix.reshape(-1, matrix.shape[-1])
+    length = flatmat.shape[0]
+    is_dominated_by_others = np.full(length, True)
+
+    for i in range(length):
+        for j in range(length):
+            if i != j and is_dominated(flatmat[i], flatmat[j]):
+                is_dominated_by_others[i] = False
                 break
 
-    pareto_result = matrix[~is_dominated]
-    return pareto_result
+    pareto_optimal_points = flatmat[is_dominated_by_others]
+    return pareto_optimal_points
+
 
 matrix = np.array([[[2, 1], [0, 0]], 
-                   [[0, 0], [1, 2]]])
+                [[0, 0], [1, 2]]])
 
-pareto_result = pareto(matrix.reshape(-1, matrix.shape[-1]))
-
-print("Pareto:")
-print(pareto_result)
+pareto_optimal_points = find_pareto_optimal(matrix)
+print(pareto_optimal_points)
